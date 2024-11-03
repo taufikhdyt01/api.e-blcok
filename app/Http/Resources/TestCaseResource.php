a24f4010dfc550f58f53ddac8ab234af213e3b01
Taufik Hidayat <taufikhdyt@student.ub.ac.id>
2024-11-03 23:11:09 +0700

    fix: expected_output test case

--- app/Http/Resources/TestCaseResource.php
+++ app/Http/Resources/TestCaseResource.php
@@ -12,26 +12,8 @@ class TestCaseResource extends JsonResource
         return [
             'id' => $this->id,
             'input' => $this->input,
-            'expected_output' => $this->formatExpectedOutput($this->expected_output),
+            'expected_output' => $this->expected_output,
             'is_sample' => $this->is_sample,
         ];
-    }
-
-    private function formatExpectedOutput($value)
-    {
-        if ($value === null) {
-            return null;
-        }
-
-        $decodedValue = json_decode($value, true);
-        if (json_last_error() === JSON_ERROR_NONE) {
-            $value = $decodedValue;
-        }
-
-        if (is_numeric($value)) {
-            return $value + 0;
-        }
-
-        return $value;
     }
 }
